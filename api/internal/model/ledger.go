d1187eda9d6e7f9fda689f4fe265f08e9a861b3a
vembry <airstrip99@gmail.com>
2023-08-07 19:58:35 +0700

    chore(api): update models

--- api/internal/model/ledger.go
+++ api/internal/model/ledger.go
@@ -1,5 +1,32 @@
 package model
 
+import (
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
+
 // CreateLedgerEntry contain fields to create a ledger entry
 type CreateLedgerEntry struct {
+	UserId      ksuid.KSUID `json:"user_id" db:"user_id"`
+	Type        string      `json:"type" db:"type"`
+	Description string      `json:"description" db:"description"`
+	Amount      float64     `json:"amount" db:"amount"`
 }
+
+// Ledger contain fields of ledger. The fields refers to 'ledgers' table
+type Ledger struct {
+	Id            ksuid.KSUID `json:"id" db:"id"`
+	UserId        ksuid.KSUID `json:"user_id" db:"user_id"`
+	Type          string      `json:"type" db:"type"`
+	Description   string      `json:"description" db:"description"`
+	Amount        float64     `json:"amount" db:"amount"`
+	BalanceAfter  float64     `json:"balance_after" db:"balance_after"`
+	BalanceBefore float64     `json:"balance_before" db:"balance_before"`
+	CreatedAt     time.Time   `json:"create_at" db:"created_at"`
+}
+
+const (
+	LedgerTypeIn  string = "in"
+	LedgerTypeOut string = "out"
+)
--- api/internal/model/transaction.go
+++ api/internal/model/transaction.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
 
 // CreateTransaction contain fields to create a transaction entry
 type CreateTransaction struct {
@@ -11,14 +15,14 @@ type CreateTransaction struct {
 
 // Transaction contain fields of transactions. The fields refers to 'transactions' table
 type Transaction struct {
-	Id          string    `json:"id" db:"id"`
-	UserId      string    `json:"user_id" db:"user_id"`
-	Status      string    `json:"status" db:"status"`
-	Description string    `json:"description" db:"description"`
-	Remarks     string    `json:"remarks" db:"remarks"`
-	Amount      float64   `json:"amount" db:"amount"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at" db:"updated_at"`
+	Id          ksuid.KSUID `json:"id" db:"id"`
+	UserId      ksuid.KSUID `json:"user_id" db:"user_id"`
+	Status      string      `json:"status" db:"status"`
+	Description string      `json:"description" db:"description"`
+	Remarks     string      `json:"remarks" db:"remarks"`
+	Amount      float64     `json:"amount" db:"amount"`
+	CreatedAt   time.Time   `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at" db:"updated_at"`
 }
 
 // containing transaction's status enum
