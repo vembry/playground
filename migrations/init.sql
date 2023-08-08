ce764a7a3420a0a73f811b17478474d2166f863e
vembry <airstrip99@gmail.com>
2023-08-09 01:18:10 +0700

    chore(migrations): tidy cause of ocds

--- migrations/init.sql
+++ migrations/init.sql
@@ -53,6 +53,42 @@ CREATE TABLE public.transactions (
 -- index transactions' status
 CREATE INDEX idx_transactions_status ON public.transactions (status);
 
+-- create balances table
+CREATE TABLE public.balances (
+    id CHAR(27) PRIMARY KEY,
+    user_id CHAR(27),
+    amount NUMERIC(15, 2),
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp,
+
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+-- setup pre-balance
+INSERT INTO public.balances(id, user_id, amount)
+VALUES
+    ('2TeSprhp2cN6nEIcayZsjjvnlsK', '2TWlPQ2AhstX9PtJ5UTOE6xQ7Ga', 1000),
+    ('2TeSppzLaJaxldlzMOkqYO37vqw', '2TWlPVmPjhonQ2DpOFt09O990th', 1000),
+    ('2TeSprCLB0tF6HsJT9eCb1IBht0', '2TWlPdYWFbP3iXPMIKVRmdZ3ozC', 1000),
+    ('2TeSps1ECSPx2IRrWMKEd6oHvSJ', '2TWlPkjd1YcRDCBDQk11nygVDpe', 1000),
+    ('2TeSpqL5Cq3t0rNJ4RDcR4ky029', '2TWlPw3U64nhEtzeazP5ELd7q4c', 1000),
+    ('2TeSpo6Okj6BedA62PUO3nRYADm', '2TWlQ2JWBetv9MUkFsLPd4zhLa4', 1000),
+    ('2TeSprwUG0oLfgGbIIObipgf5be', '2TWlQ83iEHKIOtvMCfSnBwM2sEB', 1000),
+    ('2TeSpqi0vPnYFf82cdBCE4Gaxjx', '2TWlQJsEhRIW8XpeGGz2u75phWN', 1000),
+    ('2TeSppFijPxu2bIKpRLhLy3ye4j', '2TWlQRnYNr5ViFi0wLTatYImXz5', 1000),
+    ('2TeSpnNHCn0Yq0vJddsv0dcSviy', '2TWlQYYZVIC566T3XFVldQckPsB', 1000),
+    ('2TeSpsBbTYE5ZBe3zpO1HDRf44o', '2TWlQcFLvqE67A2qbZpWAdSJiZL', 1000),
+    ('2TeSprmZUc7HGpqlm7bUi6RB9lu', '2TWlQjcMepYBrRJRRzwgXWLA0gX', 1000),
+    ('2TeSpqKkgW9pz15PaqR8ZIS0Wjh', '2TWlQvGoGcHxUa4iod28bMsfW7e', 1000),
+    ('2TeSpqfWmsmNLffPB4fhErgYRiC', '2TWlR5WFe7VUVMSxhgpEmfqlAAX', 1000),
+    ('2TeSpnH3d9Hr9zPyNpoBrzdd8fx', '2TWlR8ifwogFmktTwET0Eb2s4PE', 1000),
+    ('2TeSpsz6jeis3vJHCoSfFrnGPVl', '2TWlRFfCwsZo903aTO7xSRCYQIU', 1000),
+    ('2TeSptEF7KEwZ9cdlowOH4HSmbR', '2TWlRRtrVofuy7C1ZzcWIICVEME', 1000),
+    ('2TeSpsvjaufyDq78BwcFFZ6ibfi', '2TWlRZBGQlbQe34dVNU3GhQKshe', 1000),
+    ('2TeSpsFLvMYhPU3CpC21wgyOcS5', '2TWlRedfruxmFYvYLJur7oGesXY', 1000),
+    ('2TeSpmhKLpqgNXTNOvh3JPLkbjy', '2TWlRnR3C0hopS7NkcwyjIOq5Kd', 1000)
+;
+
 -- construct ledger_entry_type enum
 CREATE TYPE ledger_entry_type AS ENUM ('in','out');
 
@@ -96,42 +132,4 @@ VALUES
     ('2TWoNlmcEqb6pM0niSL1WgViSsD', '2TWlRZBGQlbQe34dVNU3GhQKshe', 'in', 'init balance', 0, 0, 0),
     ('2TWoNiKJghuwkEANCZ1lavtOBMw', '2TWlRedfruxmFYvYLJur7oGesXY', 'in', 'init balance', 0, 0, 0),
     ('2TWoNiinHa0FTsSPIjzMvIww10k', '2TWlRnR3C0hopS7NkcwyjIOq5Kd', 'in', 'init balance', 0, 0, 0)
-;
-
--- create balances table
-CREATE TABLE public.balances (
-    id CHAR(27) PRIMARY KEY,
-    user_id CHAR(27),
-    amount NUMERIC(15, 2),
-    created_at TIMESTAMP DEFAULT current_timestamp,
-    updated_at TIMESTAMP DEFAULT current_timestamp,
-    latest_ledger_point_id CHAR(27),
-
-    FOREIGN KEY (user_id) REFERENCES users (id),
-    FOREIGN KEY (latest_ledger_point_id) REFERENCES ledgers (id)
-);
-
--- setup pre-balance
-INSERT INTO public.balances(id, user_id, amount, latest_ledger_point_id)
-VALUES
-    ('2TeSprhp2cN6nEIcayZsjjvnlsK', '2TWlPQ2AhstX9PtJ5UTOE6xQ7Ga', 0, '2TWoNoIElOA5lEMz2YJ4hBtWKEW'),
-    ('2TeSppzLaJaxldlzMOkqYO37vqw', '2TWlPVmPjhonQ2DpOFt09O990th', 0, '2TWoNk4xFevVLpBa9CGcOlJMMtD'),
-    ('2TeSprCLB0tF6HsJT9eCb1IBht0', '2TWlPdYWFbP3iXPMIKVRmdZ3ozC', 0, '2TWoNjy0ZwMeCVJiSbUdEGD2rOF'),
-    ('2TeSps1ECSPx2IRrWMKEd6oHvSJ', '2TWlPkjd1YcRDCBDQk11nygVDpe', 0, '2TWoNlkQxwgYo7mK5SmHKhGgn7H'),
-    ('2TeSpqL5Cq3t0rNJ4RDcR4ky029', '2TWlPw3U64nhEtzeazP5ELd7q4c', 0, '2TWoNjNrMz5Tk7KWrUoyzWd9n1x'),
-    ('2TeSpo6Okj6BedA62PUO3nRYADm', '2TWlQ2JWBetv9MUkFsLPd4zhLa4', 0, '2TWoNlsyvef315nycZp2Fo38ir2'),
-    ('2TeSprwUG0oLfgGbIIObipgf5be', '2TWlQ83iEHKIOtvMCfSnBwM2sEB', 0, '2TWoNkSFocY94EtT1bzah03YW7M'),
-    ('2TeSpqi0vPnYFf82cdBCE4Gaxjx', '2TWlQJsEhRIW8XpeGGz2u75phWN', 0, '2TWoNjXHN5wO1mCkKHWrmeqHXwl'),
-    ('2TeSppFijPxu2bIKpRLhLy3ye4j', '2TWlQRnYNr5ViFi0wLTatYImXz5', 0, '2TWoNnfgdYCl6RP8uuId2UxiPV4'),
-    ('2TeSpnNHCn0Yq0vJddsv0dcSviy', '2TWlQYYZVIC566T3XFVldQckPsB', 0, '2TWoNkrfVNVHZJiuRQse59VuKK9'),
-    ('2TeSpsBbTYE5ZBe3zpO1HDRf44o', '2TWlQcFLvqE67A2qbZpWAdSJiZL', 0, '2TWoNkcUykDQ4sBOAy3IwLgGz0t'),
-    ('2TeSprmZUc7HGpqlm7bUi6RB9lu', '2TWlQjcMepYBrRJRRzwgXWLA0gX', 0, '2TWoNilW0BrGwmP95XZlIpN6zEg'),
-    ('2TeSpqKkgW9pz15PaqR8ZIS0Wjh', '2TWlQvGoGcHxUa4iod28bMsfW7e', 0, '2TWoNmxDzxJvxVwxv7MQleJ5XL3'),
-    ('2TeSpqfWmsmNLffPB4fhErgYRiC', '2TWlR5WFe7VUVMSxhgpEmfqlAAX', 0, '2TWoNju27StnXldwLZwKvhEV53V'),
-    ('2TeSpnH3d9Hr9zPyNpoBrzdd8fx', '2TWlR8ifwogFmktTwET0Eb2s4PE', 0, '2TWoNm6hVYSFmLA87XBe9UPU8NT'),
-    ('2TeSpsz6jeis3vJHCoSfFrnGPVl', '2TWlRFfCwsZo903aTO7xSRCYQIU', 0, '2TWoNiySHd5cook6r4qWbpWBmmh'),
-    ('2TeSptEF7KEwZ9cdlowOH4HSmbR', '2TWlRRtrVofuy7C1ZzcWIICVEME', 0, '2TWoNmYo3Tnc9isXPcL6xKRzvlp'),
-    ('2TeSpsvjaufyDq78BwcFFZ6ibfi', '2TWlRZBGQlbQe34dVNU3GhQKshe', 0, '2TWoNlmcEqb6pM0niSL1WgViSsD'),
-    ('2TeSpsFLvMYhPU3CpC21wgyOcS5', '2TWlRedfruxmFYvYLJur7oGesXY', 0, '2TWoNiKJghuwkEANCZ1lavtOBMw'),
-    ('2TeSpmhKLpqgNXTNOvh3JPLkbjy', '2TWlRnR3C0hopS7NkcwyjIOq5Kd', 0, '2TWoNiinHa0FTsSPIjzMvIww10k')
 ;
