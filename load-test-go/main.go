70a0e5576f967d3ed40fa06b73dc5a8d165357c7
vembry <airstrip99@gmail.com>
2024-06-24 19:33:55 +0000

    chore(load-test): add tracer + compile everything in 1 file

--- load-test-go/main.go
+++ load-test-go/main.go
@@ -1,10 +1,38 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
-	"math/rand"
+	"io"
+	"log"
+	"log/slog"
+	"math/rand/v2"
+	"net/http"
+	"os"
 	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 )
+
+type response[T any] struct {
+	Error  string `json:"error"`
+	Object T      `json:"object"`
+}
+
+type balance struct {
+	Id        string    `json:"id"`
+	Amount    float64   `json:"amount"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+const AppHost string = "http://host.docker.internal:8080"
 
 var balanceIds []string = []string{
 	"2TeSprhp2cN6nEIcayZsjjvnlsK",
@@ -49,30 +77,190 @@ var balanceIds []string = []string{
 	// "2TWlRnR3C0hopS7NkcwyjIOq5Kd",
 }
 
+type traceHandler struct {
+	slog.Handler
+}
+
+func (h *traceHandler) Handle(ctx context.Context, r slog.Record) error {
+	span := trace.SpanFromContext(ctx)
+	if span.SpanContext().IsValid() {
+		r.AddAttrs(
+			slog.String("trace_id", span.SpanContext().TraceID().String()),
+			slog.String("span_id", span.SpanContext().SpanID().String()),
+		)
+	}
+	return h.Handler.Handle(ctx, r)
+}
+
 func main() {
+
+	// exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	// if err != nil {
+	// 	log.Fatalf("error initiating trace. err=%v", err)
+	// }
+
+	tp := sdktrace.NewTracerProvider(
+		// sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("load-test"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+
+	defer (func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			log.Fatalf("Error shutting down tracer provider: %v", err)
+		}
+	})()
+
+	tracer := otel.Tracer("load-test-tracer")
+
 	end := time.Now().Add(1 * time.Minute)
 	fmt.Println("starting")
 
 	for time.Now().Before(end) {
 		for _, balanceId := range balanceIds {
-			val := rand.Float64() * 1000
+			ctx, span := tracer.Start(context.Background(), "main")
+			defer span.End()
+
+			handler := slog.NewJSONHandler(os.Stdout, nil)
+			customHandler := &traceHandler{Handler: handler}
+			logger := slog.New(customHandler)
+
+			// logger.InfoContext(ctx, "starting for", slog.String("balanceId", balanceId))
 
 			// get balance
-			bal := GetBalance(balanceId)
+			bal, err := GetBalance(ctx, balanceId)
+			if err != nil {
+				logger.ErrorContext(ctx, "got error on get-balance. err=%v", slog.Any("error", err))
+				continue
+			}
 			if bal == nil {
+				logger.ErrorContext(ctx, "get-balance return nil")
 				continue
 			}
 
-			// deposit money when lacking
-			if bal.Amount < val {
-				val2 := 100 + rand.Float64()*10000
-				Deposit(balanceId, val2)
+			amount := rand.Float64() * 1000
+
+			// deposit money when needed
+			if bal.Amount < amount {
+				depositAmount := 100 + rand.Float64()*10000
+				err := Deposit(ctx, balanceId, depositAmount)
+				if err != nil {
+					logger.ErrorContext(ctx, "got error on deposit. err=%v", slog.Any("error", err))
+					continue
+				}
 			}
 
 			// exec withdrawal
-			Withdraw(balanceId, val)
+			err = Withdraw(ctx, balanceId, amount)
+			if err != nil {
+				logger.ErrorContext(ctx, "got error on withdrawal. err=%v", slog.Any("error", err))
+				continue
+			}
 		}
 	}
 
 	fmt.Println("finished")
+}
+
+func Deposit(ctx context.Context, balanceId string, amount float64) error {
+	payloadRaw, _ := json.Marshal(map[string]interface{}{
+		"amount": amount,
+	})
+
+	req, _ := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		fmt.Sprintf("%s/balance/%s/deposit", AppHost, balanceId),
+		bytes.NewBuffer(payloadRaw),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("error on http request. error=%w", err)
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Printf("depositing balance '%s' return non 200", balanceId)
+		return err
+	}
+
+	// rawbody, err := io.ReadAll(res.Body)
+	// if err != nil {
+	// 	log.Printf("error on reading body. error=%v", err)
+	// 	return err
+	// }
+
+	// var out response[balance]
+	// _ = json.Unmarshal(rawbody, &out)
+
+	return nil
+}
+
+func GetBalance(ctx context.Context, balanceId string) (*balance, error) {
+	req, _ := http.NewRequestWithContext(
+		ctx,
+		"GET",
+		fmt.Sprintf("%s/balance/%s", AppHost, balanceId),
+		nil,
+	)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error on http request. error=%w", err)
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get balance '%s' return non 200", balanceId)
+	}
+
+	rawbody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error on reading body. error=%w", err)
+	}
+
+	var out response[balance]
+	_ = json.Unmarshal(rawbody, &out)
+
+	return &out.Object, nil
+}
+
+func Withdraw(ctx context.Context, balanceId string, amount float64) error {
+	payloadRaw, _ := json.Marshal(map[string]interface{}{
+		"amount": amount,
+	})
+
+	req, _ := http.NewRequestWithContext(
+		ctx,
+		"POST",
+		fmt.Sprintf("%s/balance/%s/withdraw", AppHost, balanceId),
+		bytes.NewBuffer(payloadRaw),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("error on http request. error=%w", err)
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		log.Printf("depositing balance '%s' return non 200", balanceId)
+		return err
+	}
+
+	// rawbody, err := io.ReadAll(res.Body)
+	// if err != nil {
+	// 	log.Printf("error on reading body. error=%v", err)
+	// 	return err
+	// }
+
+	// var out response[balance]
+	// _ = json.Unmarshal(rawbody, &out)
+
+	return nil
 }
