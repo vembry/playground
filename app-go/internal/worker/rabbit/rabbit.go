16b563136ab15f43fc508215ce1284002947464f
vembry <airstrip99@gmail.com>
2024-07-31 02:45:23 +0000

    refactor(app-go): modularize rabbit starter

--- app-go/internal/worker/rabbit/rabbit.go
+++ app-go/internal/worker/rabbit/rabbit.go
@@ -46,55 +46,63 @@ func (r *rabbit) Start() {
 	for _, consumer := range r.consumers {
 		log.Printf("rabbit: starting '%s' consumer", consumer.Name())
 
-		// declare queue in case it is missing
-		// for now the config will be defined here
-		// until further cases
-		_, err := consumer.Channel().QueueDeclare(
-			consumer.Name(), // name
-			false,           // durable
-			false,           // delete when unused
-			false,           // exclusive
-			false,           // no-wait
-			nil,             // arguments
-		)
+		r.startConsumer(consumer)
+	}
+}
+
+// startConsumer is to start consumer by prepping needed preps
+func (r *rabbit) startConsumer(consumer iConsumer) {
+	// declare queue in case it is missing
+	// for now the config will be defined here
+	// until further cases
+	_, err := consumer.Channel().QueueDeclare(
+		consumer.Name(), // name
+		false,           // durable
+		false,           // delete when unused
+		false,           // exclusive
+		false,           // no-wait
+		nil,             // arguments
+	)
+	if err != nil {
+		log.Fatalf("rabbit: failed to declare a queue. consumer=%s. err=%v", consumer.Name(), err)
+	}
+
+	// is this proper?
+	messageCh, err := consumer.Channel().Consume(
+		consumer.Name(), // queue
+		"",              // consumer
+		false,           // auto-ack
+		false,           // exclusive
+		false,           // no-local
+		false,           // no-wait
+		nil,             // args
+	)
+
+	if err != nil {
+		log.Fatalf("rabbit: failed to setup message consumer. consumer=%s. err=%v", consumer.Name(), err)
+	}
+
+	// start consuming
+	go r.consumerConsume(consumer, messageCh)
+}
+
+// consumerConsume start the actual message consumption
+func (r *rabbit) consumerConsume(consumer iConsumer, messageCh <-chan amqp.Delivery) {
+	for message := range messageCh {
+		log.Printf("'%s' consuming %s", consumer.Name(), string(message.Body))
+
+		// consume incoming message
+		err := consumer.Handle(context.Background(), message)
 		if err != nil {
-			log.Fatalf("rabbit: failed to declare a queue. consumer=%s. err=%v", consumer.Name(), err)
-		}
+			log.Printf("rabbit: failed to handle. consumer=%s. err=%v", consumer.Name(), err)
 
-		// is this proper?
-		messageCh, err := consumer.Channel().Consume(
-			consumer.Name(), // queue
-			"",              // consumer
-			false,           // auto-ack
-			false,           // exclusive
-			false,           // no-local
-			false,           // no-wait
-			nil,             // args
-		)
-
-		if err != nil {
-			log.Fatalf("rabbit: failed to setup message consumer. consumer=%s. err=%v", consumer.Name(), err)
-		}
-
-		// start consuming
-		go func() {
-			for message := range messageCh {
-				log.Printf("'%s' consuming %s", consumer.Name(), string(message.Body))
-
-				// consume incoming message
-				err := consumer.Handle(context.Background(), message)
-				if err != nil {
-					log.Printf("rabbit: failed to handle. consumer=%s. err=%v", consumer.Name(), err)
-
-					err = message.Reject(true)
-					if err != nil {
-						log.Printf("rabbit: failed to reject. consumer=%s. err=%v", consumer.Name(), err)
-					}
-				} else {
-					message.Ack(true)
-				}
+			err = message.Reject(true)
+			if err != nil {
+				log.Printf("rabbit: failed to reject. consumer=%s. err=%v", consumer.Name(), err)
 			}
-		}()
+		} else {
+			message.Ack(true)
+		}
 	}
 }
 
