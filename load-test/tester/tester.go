700b24a81d9f7700d62bfe8c6aa970b94facb36e
vembry <airstrip99@gmail.com>
2024-08-02 04:00:09 +0000

    chore(load-test): isolate inner part of actual execution

--- load-test/tester/tester.go
+++ load-test/tester/tester.go
@@ -31,25 +31,26 @@ func New[T any](cfg Config, parameter T) *tester[T] {
 // Do setup and execute load-tester
 func (t *tester[T]) Do(execution func(ctx context.Context, l *slog.Logger, parameter T)) {
 
-	// setup otel trace
+	// load otel trace
 	tracer := otel.Tracer("load-test-tracer")
-
-	// tracer := otel.GetTracerProvider().Tracer("asd")
 
 	// setup end time
 	end := time.Now().Add(t.cfg.Duration)
 
 	for time.Now().Before(end) {
-		// start open-telemetry
-		ctx, span := tracer.Start(
-			context.Background(),
-			"load-test-starter",
-			trace.WithSpanKind(trace.SpanKindServer),
-		)
-		defer span.End()
-
-		// execute test script
-		execution(ctx, t.cfg.Logger, t.parameter)
+		t.do(tracer, execution)
 	}
 
+}
+
+func (t *tester[T]) do(tracer trace.Tracer, execution func(ctx context.Context, l *slog.Logger, parameter T)) {
+	// start open-telemetry
+	ctx, span := tracer.Start(
+		context.Background(),
+		"load-test-starter",
+	)
+	defer span.End()
+
+	// execute test script
+	execution(ctx, t.cfg.Logger, t.parameter)
 }
