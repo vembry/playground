4163ce161e84a9358b8c65bcb75d93f779ec9c46
vembry <airstrip99@gmail.com>
2024-09-30 17:39:38 +0000

    refactor(app-java): remove unused import

--- app-java/src/main/java/vembry/playground/app/models/Withdrawal.java
+++ app-java/src/main/java/vembry/playground/app/models/Withdrawal.java
@@ -2,7 +2,6 @@ package vembry.playground.app.models;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.ksuid.Ksuid;
