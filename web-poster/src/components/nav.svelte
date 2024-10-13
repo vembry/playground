7545c3783d467f01a726c7e2b412539ecae8d088
vembry <airstrip99@gmail.com>
2024-10-13 06:26:11 +0000

    chore(web-poster): user management

--- web-poster/src/components/nav.svelte
+++ web-poster/src/components/nav.svelte
@@ -1,12 +1,48 @@
+<script lang="ts">
+	import { getUserId, setUserId } from '$lib/user';
+	import { onMount } from 'svelte';
+	let userId: string = '';
+	let isEditName: boolean = false;
+	onMount(() => {
+		userId = getUserId();
+	});
+</script>
+
 <div class="navbar">
-	<ul>
-		<a href="/">
-			<li>home</li>
-		</a>
-		<a href="/posts">
-			<li>posts</li>
-		</a>
-	</ul>
+	<div>
+		hi <strong>{userId}</strong>!
+		{#if !isEditName}
+			<button
+				on:click={(e) => {
+					isEditName = !isEditName;
+				}}>edit name</button
+			>
+		{/if}
+	</div>
+	{#if isEditName}
+		<div>
+			<form>
+				<input type="text" bind:value={userId} />
+				<button
+					on:click={(e) => {
+						setUserId(userId);
+						isEditName = !isEditName;
+						userId = userId;
+					}}>save</button
+				>
+			</form>
+		</div>
+	{/if}
+	<div>
+		<ul>
+			<a href="/">
+				<li>home</li>
+			</a>
+			<a href="/posts">
+				<li>posts</li>
+			</a>
+		</ul>
+	</div>
 </div>
 
 <style>
--- web-poster/src/lib/user.ts
+++ web-poster/src/lib/user.ts
@@ -1,11 +1,12 @@
 import { v7 as uuid7 } from 'uuid';
+import Cookie from 'js-cookie';
 
 const USER_ID: string = 'user_id';
 export function getUserId(): string {
-	if (typeof window !== 'undefined') {
-		const userId = sessionStorage.getItem(USER_ID) || uuid7();
-		sessionStorage.setItem(USER_ID, uuid7());
-		return userId;
-	}
-	return ""
+	const userId = Cookie.get(USER_ID) || uuid7();
+	setUserId(userId);
+	return userId;
+}
+export function setUserId(userId: string) {
+	Cookie.set(USER_ID, userId);
 }
