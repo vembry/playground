81b9b91791434d483b218332f9259fc7896c5cf0
vembry <airstrip99@gmail.com>
2025-01-19 00:47:19 +0000

    chore(app-csharp): tidy code

--- app-csharp/Program.cs
+++ app-csharp/Program.cs
@@ -1,11 +1,23 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "app-csharp" });
+});
 
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "app-csharp API V1");
+   });
+}
 
 app.MapControllers();
 
